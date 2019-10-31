ced644a1b98a4db2c7e3f52e5ff9b0e6cb2995e2
JonathanMelcher <melcher83@gmail.com>
2019-10-31 15:29:58 -0500

    Basic Gui, does nothing yet

--- SNMP-GUI.py
+++ SNMP-GUI.py
@@ -1,5 +1,67 @@
-from PyQt5.QtWidgets import QApplication, QLabel
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec_()
+# -*- coding: utf-8 -*-
+
+
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(690, 529)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(320, 480, 341, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.listView = QtWidgets.QListView(Dialog)
+        self.listView.setGeometry(QtCore.QRect(285, 40, 381, 401))
+        self.listView.setObjectName("listView")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(140, 50, 113, 22))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(140, 90, 113, 22))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(70, 50, 55, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(70, 90, 121, 16))
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(170, 160, 75, 24))
+        self.pushButton.setObjectName("pushButton")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(70, 120, 121, 16))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_3.setGeometry(QtCore.QRect(140, 120, 113, 22))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Host"))
+        self.label_2.setText(_translate("Dialog", "Community"))
+        self.pushButton.setText(_translate("Dialog", "OID GET"))
+        self.label_3.setText(_translate("Dialog", "OID"))
+
+app=QApplication(sys.argv)
+window=QDialog()
+ui=Ui_Dialog()
+ui.setupUi(window)
+window.show()
+sys.exit(app.exec_())
